dead79747406de47a8fe4e08260b1650e5948a5b
GisellePiasetzki <piasetzki.giselle@gmail.com>
2021-11-23 09:51:53 -0300

    criado projeto

---
+++ jenkins-tdois2/src/main/java/com/example/jenkinstdois2/JenkinsTdois2Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstdois2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTdois2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTdois2Application.class, args);
+	}
+
+}
---
+++ jenkins-tdois2/src/test/java/com/example/jenkinstdois2/JenkinsTdois2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstdois2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTdois2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
